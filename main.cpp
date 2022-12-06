7666319dcf2586bd02b88c84b758a15a45c959f5
Rarka1-01 <artoym.kalinichenko@yandex.ru>
2022-12-06 18:48:39 +0700

    tests for rb_tree

--- main.cpp
+++ main.cpp
@@ -1,23 +1,41 @@
 #include "lib/header.h"
+#include <fstream>
 
 void test()
 {
+    int size[] = {10, 50, 100, 250, 300, 450, 500, 650, 700, 750};
+
+    ofstream f_r_i("D:/FFFFFFFFFFFFFFFFFFFFFFFFFFFF/rb_ins.txt");
+    ofstream f_r_r("D:/FFFFFFFFFFFFFFFFFFFFFFFFFFFF/rb_rem.txt");
+    ofstream f_r_s("D:/FFFFFFFFFFFFFFFFFFFFFFFFFFFF/rb_see.txt");
+
+    for(int i = 0; i < 10; i++)
+    {
+        RBTree<int, int> rbT;
+        for(int j = 0; j < size[i]; j++)
+        {
+            rbT.put(j, 2);
+        }
+
+        f_r_i << rbT.getTreeNodesCountViewedByPreviousOperation() << endl;
+
+        rbT.getValueByKey(size[i] / 2);
+
+        f_r_s << rbT.getTreeNodesCountViewedByPreviousOperation() << endl;
+
+        rbT.removeNodeByKey(size[i] / 2);
+
+        f_r_r << rbT.getTreeNodesCountViewedByPreviousOperation() << endl;
+    }
+
+    f_r_i.close();
+    f_r_r.close();
+    f_r_s.close();
 
 }
 
 int main() {
-    RBTree<int, int> rbTree;
-    for (int i = 1; i < 17; ++i) {
-        rbTree.put(i, 3);
-    }
-    rbTree.print();
-    cout << endl;
-    cout << endl;
 
-    cout << rbTree.getSize() << endl;
-    cout << rbTree.getValueByKey(6) << endl;
-
-//    rbTree.print();
-
+    test();
     return 0;
 }
--- src/rb_tree.cpp
+++ src/rb_tree.cpp
@@ -7,8 +7,10 @@ RBTree<K, V>::RBTree() {
 
 template<typename K, typename V>
 bool RBTree<K, V>::put(K key, V value) {
+    this->nodes_counter = 0;
     auto *node = new RBTreeNode<K, V>(key, value);
     this->root = insert(this->root, node);
+    this->nodes_counter++;
     this->size++;
     return true;
 }
@@ -22,6 +24,7 @@ RBTreeNode<K, V> *RBTree<K, V>::insert(RBTreeNode<K, V> *root, RBTreeNode<K, V> 
     root = n;
     while (root->getParent() != nullptr) {
         root = root->getParent();
+        this->nodes_counter++;
     }
     return root;
 }
@@ -43,6 +46,7 @@ template<typename K, typename V>
 void RBTree<K, V>::insert_case1(RBTreeNode<K, V> *n) {
     if (n->getParent() == nullptr) {
         n->setColor(BLACK);
+        this->nodes_counter++;
     }
 }
 
@@ -55,6 +59,7 @@ void RBTree<K, V>::insert_case3(RBTreeNode<K, V> *n) {
     n->getParent()->setColor(BLACK);
     uncle(n)->setColor(BLACK);
     grandparent(n)->setColor(RED);
+    this->nodes_counter += 2;
     insert_repair_tree(grandparent(n));
 }
 
@@ -86,6 +91,7 @@ void RBTree<K, V>::insert_case4step2(RBTreeNode<K, V> *n) {
     }
     parent->setColor(BLACK);
     g->setColor(RED);
+    this->nodes_counter += 2;
 }
 
 template<typename K, typename V>
@@ -96,21 +102,25 @@ void RBTree<K, V>::rotate_left(RBTreeNode<K, V> *n) {
     n->setRight(nnew->getLeft());
     nnew->setLeft(n);
     n->setParent(nnew);
+    this->nodes_counter += 4;
 
-    // handle other child/parent pointers
     if (n->getRight() != nullptr) {
         n->getRight()->setParent(n);
+        this->nodes_counter += 2;
     }
 
-    if (p != nullptr) // initially n could be the root
+    if (p != nullptr)
     {
         if (n == p->getLeft()) {
             p->setLeft(nnew);
+            this->nodes_counter++;
         } else if (n == p->getRight()) {
             p->setRight(nnew);
+            this->nodes_counter++;
         }
     }
     nnew->setParent(p);
+    this->nodes_counter++;
 }
 
 template<typename K, typename V>
@@ -118,26 +128,29 @@ void RBTree<K, V>::rotate_right(RBTreeNode<K, V> *n) {
     RBTreeNode<K, V> *nnew = n->getLeft();
     RBTreeNode<K, V> *p = n->getParent();
 
-    //fixme
-    // assert(nnew != NULL); // since the leaves of a red-black tree are empty, they cannot become internal nodes
     n->setLeft(nnew->getRight());
     nnew->setRight(n);
     n->setParent(nnew);
-    // handle other child/parent pointers
+    this->nodes_counter += 4;
     if (n->getLeft() != nullptr) {
         n->getLeft()->setParent(n);
+        this->nodes_counter += 2;
 
     }
-    if (p != nullptr) // initially n could be the root
+    if (p != nullptr)
     {
-        if (n == p->getLeft())
+        if (n == p->getLeft()) {
             p->setLeft(nnew);
+            this->nodes_counter++;
+        }
         else if (n == p->getRight()) {
             p->setRight(nnew);
+            this->nodes_counter++;
         }
 
     }
     nnew->setParent(p);
+    this->nodes_counter++;
 }
 
 template<typename K, typename V>
@@ -148,6 +161,7 @@ void RBTree<K, V>::insert_recurse(RBTreeNode<K, V> *root, RBTreeNode<K, V> *n) {
             return;
         } else {
             root->setLeft(n);
+            this->nodes_counter++;
         }
     } else if (root != nullptr) {
         if (!(isLeaf(root->getRight()))) {
@@ -155,10 +169,12 @@ void RBTree<K, V>::insert_recurse(RBTreeNode<K, V> *root, RBTreeNode<K, V> *n) {
             return;
         } else {
             root->setRight(n);
+            this->nodes_counter++;
         }
     }
 
     // insert new n n
+    this->nodes_counter++;
     n->setParent(root);
     n->setLeft(new RBTreeNode<K, V>());
     leaf(n->getLeft());
@@ -213,6 +229,7 @@ bool RBTree<K, V>::removeNodeByKey(K key) {
         replaceNode(n, child);
         if (n->getColor() == BLACK) {
             if (child->getColor() == RED) {
+                this->nodes_counter++;
                 child->setColor(BLACK);
             } else {
                 delete_case1(child);
@@ -240,6 +257,7 @@ void RBTree<K, V>::delete_case2(RBTreeNode<K, V> *n) {
     RBTreeNode<K, V> *s = sibling(n);
 
     if (s->getColor() == RED) {
+        this->nodes_counter++;
         n->getParent()->setColor(RED);
         s->setColor(BLACK);
         if (n == n->getParent()->getLeft())
@@ -273,6 +291,7 @@ void RBTree<K, V>::delete_case4(RBTreeNode<K, V> *n) {
         (s->getColor() == BLACK) &&
         (s->getLeft()->getColor() == BLACK) &&
         (s->getRight()->getColor() == BLACK)) {
+        this->nodes_counter += 2;
         s->setColor(RED);
         n->getParent()->setColor(BLACK);
     } else
@@ -284,86 +303,17 @@ void RBTree<K, V>::delete_case5(RBTreeNode<K, V> *n) {
     RBTreeNode<K, V> *s = sibling(n);
 
     if (s->getColor() == 'b') {
-        /*this
-        if statement
-            is trivial,
-                    due
-            to
-            case 2(even though
-            case 2 changed
-                        the
-                sibling
-                        to
-                a
-                        sibling
-                's child,
-        the
-                sibling
-        's child can't
-        be
-                red, since
-        no
-                red
-        parent
-                can
-        have
-                a
-        red
-        child).
-        the
-        following
-                statements
-        just
-                force
-        the
-                red
-        to
-                be
-        on
-                the
-        left
-                of
-        the
-                left
-        of
-                the
-        parent,
-        or right
-                of
-        the
-                right, so
-        case six will
-                    rotate
-            correctly.*/
         if ((n == n->getParent()->getLeft()) &&
             (s->getRight()->getColor() == 'b') &&
             (s->getLeft()->getColor() == 'r')) {
-            /*this
-            last
-                    test
-            is
-                    trivial
-            too
-                    due
-            to
-                    cases
-            2 - 4.*/
+            this->nodes_counter += 2;
             s->setColor(RED);
             s->getLeft()->setColor(BLACK);
             rotate_right(s);
         } else if ((n == n->getParent()->getRight()) &&
                    (s->getLeft()->getColor() == BLACK) &&
                    (s->getRight()->getColor() == RED)) {
-            /*this
-            last
-                    test
-            is
-                    trivial
-            too
-                    due
-            to
-                    cases
-            2 - 4.*/
+            this->nodes_counter += 2;
             s->setColor(RED);
             s->getRight()->setColor(BLACK);
             rotate_left(s);
@@ -385,10 +335,13 @@ void RBTree<K, V>::delete_case6(RBTreeNode<K, V> *n) {
         s->getLeft()->setColor(BLACK);
         rotate_right(n->getParent());
     }
+
+    this->nodes_counter += 4;
 }
 
 template<typename K, typename V>
 bool RBTree<K, V>::isLeaf(RBTreeNode<K, V> *t) {
+    this->nodes_counter += 2;
     return (t->getLeft() == nullptr && t->getRight() == nullptr && t->getColor() == BLACK);
 }
 
@@ -405,6 +358,7 @@ RBTreeNode<K, V> *RBTree<K, V>::search(RBTreeNode<K, V> *temp, K key) {
         } else {
             return tmp;
         }
+        this->nodes_counter++;
     }
     throw "not found!";
 }
@@ -412,11 +366,14 @@ RBTreeNode<K, V> *RBTree<K, V>::search(RBTreeNode<K, V> *temp, K key) {
 template<typename K, typename V>
 void RBTree<K, V>::replaceNode(RBTreeNode<K, V> *n, RBTreeNode<K, V> *child) {
     child->setParent(n->getParent());
+    this->nodes_counter++;
     if (n == n->getParent()->getLeft()) {
         n->getParent()->setLeft(child);
     } else {
         n->getParent()->setRight(child);
     }
+
+    this->nodes_counter += 2;
 }
 
 template<typename K, typename V>
@@ -427,12 +384,14 @@ void RBTree<K, V>::nodePrint(RBTreeNode<K, V> *tmp, int n) {
         for (int i = 0; i < n; i++)
             cout << " ";
         cout << "[" << tmp->getKey() << "-" << tmp->getColor() << "]" << endl;
+        this->nodes_counter++;
         nodePrint(tmp->getLeft(), n + 5);
     }
 }
 
 template<typename K, typename V>
 void RBTree<K, V>::print() {
+    this->nodes_counter = 0;
     nodePrint(this->root, 0);
 }
 
@@ -442,14 +401,18 @@ void RBTree<K, V>::leaf(RBTreeNode<K, V> *t) {
     t->setLeft(nullptr);
     t->setParent(nullptr);
     t->setColor(BLACK);
+    this->nodes_counter++;
 }
 
 template<typename K, typename V>
 V RBTree<K, V>::getValueByKey(K key) {
+    this->nodes_counter = 0;
     if (key == this->root->getKey()) {
+        this->nodes_counter++;
         return this->root->getValue();
     }
     RBTreeNode<K, V> *curNode = this->root;
+    this->nodes_counter++;
     while (curNode != nullptr && key != curNode->getKey()) {
         if (key == curNode->getKey()) {
             return curNode->getValue();
@@ -460,9 +423,10 @@ V RBTree<K, V>::getValueByKey(K key) {
         } else {
             curNode = curNode->getRight();
         }
-
+        this->nodes_counter++;
     }
     if (curNode != nullptr) {
+        this->nodes_counter++;
         return curNode->getValue();
     } else {
         throw "not found!";
