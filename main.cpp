668be3f46fd8dc83042c635ab9b02dc4f9aa0f6b
Rarka1-01 <artoym.kalinichenko@yandex.ru>
2022-12-06 22:41:37 +0700

    Up to date tests with samogon

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@
 
 void test()
 {
-    int size[] = {10, 50, 100, 250, 300, 450, 500, 650, 700, 750};
+    int size[250];
+
+    for (int i = 1; i <= 250; i++)
+        size[i - 1] = i * 100;
 
     ofstream f_r_i("D:/FFFFFFFFFFFFFFFFFFFFFFFFFFFF/rb_ins.txt");
     ofstream f_r_r("D:/FFFFFFFFFFFFFFFFFFFFFFFFFFFF/rb_rem.txt");
     ofstream f_r_s("D:/FFFFFFFFFFFFFFFFFFFFFFFFFFFF/rb_see.txt");
 
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 250; i++)
     {
         RBTree<int, int> rbT;
         for(int j = 0; j < size[i]; j++)
@@ -17,15 +20,15 @@ void test()
             rbT.put(j, 2);
         }
 
-        f_r_i << int(rbT.getTreeNodesCountViewedByPreviousOperation() / 7) << endl;
+        f_r_i << float(rbT.getTreeNodesCountViewedByPreviousOperation()) / 6.5 << endl;
 
         rbT.getValueByKey(size[i] - 1);
 
-        f_r_s << int(rbT.getTreeNodesCountViewedByPreviousOperation() / 1.5) << endl;
+        f_r_s << float(rbT.getTreeNodesCountViewedByPreviousOperation()) / 1.8 << endl;
 
         rbT.removeNodeByKey(size[i] - 1);
 
-        f_r_r << int(rbT.getTreeNodesCountViewedByPreviousOperation() / 4.9) << endl;
+        f_r_r << float(rbT.getTreeNodesCountViewedByPreviousOperation()) / 7 << endl;
     }
 
     f_r_i.close();
